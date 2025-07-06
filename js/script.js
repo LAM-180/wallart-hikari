32d89c46352598436e07573ab75ab35bb3ab6d08
Lam <lambertye.art2@gmail.com>
2025-07-06 17:49:29 +0900

    LPのデザインを調整しました

--- js/script.js
+++ js/script.js
@@ -1,34 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Lightboxモーダルの要素を取得
-    var lightboxModal = document.getElementById('lightbox-modal');
-    var lightboxImg = document.getElementById('lightbox-img');
-    var lightboxCaption = document.getElementById('lightbox-caption');
-    var closeButton = document.getElementsByClassName('close-button')[0];
-
-    // 各グリッドアイテムの画像にクリックイベントを追加
-    document.querySelectorAll('.art-grid-item img').forEach(function(img) {
-        img.addEventListener('click', function() {
-            var imageUrl = this.src;
-            var imageAlt = this.alt;
-
-            lightboxModal.style.display = 'flex';
-            lightboxImg.src = imageUrl;
-            lightboxCaption.textContent = imageAlt;
-        });
-    });
-
-    // 閉じるボタンにクリックイベントを追加
-    closeButton.addEventListener('click', function() {
-        lightboxModal.style.display = 'none';
-    });
-
-    // モーダルの外側をクリックで閉じる
-    lightboxModal.addEventListener('click', function(event) {
-        if (event.target === lightboxModal) {
-            lightboxModal.style.display = 'none';
-        }
-    });
-
     // スムーズスクロールの処理
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
